e27ea2a182da9e8e651a2f37583a34bbbcc45181
Zorey Zvara <217864473+zroei@users.noreply.github.com>
2026-03-15 11:10:31 +0700

    Implement service worker registration
    Add service worker registration in sw-register.js

---
+++ js/sw-register.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+                console.log('ServiceWorker registered:', registration.scope);
+            })
+            .catch(err => {
+                console.log('ServiceWorker registration failed:', err);
+            });
+    });
+}
